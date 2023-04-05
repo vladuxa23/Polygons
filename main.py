595610539c6b9cd7c8fcec4acc7b386e0d035249
vladuxa23 <vlad_1995.02@mail.ru>
2023-04-05 22:14:46 +0300

    add examples

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
+from typing import List
+
 import examples
 from canvas import Canvas
 from field import Field
+from point import FieldPoint
+
+
+def draw_figures(figures, field_width=40, field_height=20):
+    for figure in figures:
+        canvas = Canvas(field_width, field_height)
+        for point in figure:
+            if point.fill == ".":
+                canvas.draw_point(point.x, point.y)
+        canvas.show()
+        print()
+
+
+def draw_figure(figure: List[FieldPoint], field_width=40, field_height=20):
+    canvas = Canvas(field_width, field_height)
+
+    for point in figure:
+        if point.fill == ".":
+            canvas.draw_point(point.x, point.y)
+    canvas.show()
+    print()
+
+
+field = Field(examples.light)
+# draw_figures(field.figures, field.width, field.height)  # Раскомментировать, чтобы распечатать обнаруженные фигуры
+print(f"Самая большая фигура площадью {len(field.max_figure)} точек")
+draw_figure(field.max_figure)
+
+field = Field(examples.medium)
+# draw_figures(field.figures, field.width, field.height)  # Раскомментировать, чтобы распечатать обнаруженные фигуры
+print(f"Самая большая фигура площадью {len(field.max_figure)} точек")
+draw_figure(field.max_figure)
+
+field = Field(examples.medium_2)
+# draw_figures(field.figures, field.width, field.height)  # Раскомментировать, чтобы распечатать обнаруженные фигуры
+print(f"Самая большая фигура площадью {len(field.max_figure)} точек")
+draw_figure(field.max_figure)
+
+field = Field(examples.hard)
+# draw_figures(field.figures, field.width, field.height)  # Раскомментировать, чтобы распечатать обнаруженные фигуры
+print(f"Самая большая фигура площадью {len(field.max_figure)} точек")
+draw_figure(field.max_figure)
 
 field = Field(examples.hard_2)
-
-# Раскомментировать, чтобы распечатать обнаруженные фигуры
-# for figure in field.figures:
-#     canvas = Canvas(field.width, field.height)
-#     for point in figure:
-#         if point.fill == ".":
-#             canvas.draw_point(point.x, point.y)
-#     canvas.show()
-#     print()
-
-
+# draw_figures(field.figures, field.width, field.height)  # Раскомментировать, чтобы распечатать обнаруженные фигуры
 print(f"Самая большая фигура площадью {len(field.max_figure)} точек")
+draw_figure(field.max_figure)
+
+field = Field(examples.hard_3)
+# draw_figures(field.figures, field.width, field.height)  # Раскомментировать, чтобы распечатать обнаруженные фигуры
+print(f"Самая большая фигура площадью {len(field.max_figure)} точек")
+draw_figure(field.max_figure)
 
