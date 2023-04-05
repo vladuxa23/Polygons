45f0772498c1dd6dcc64ad4395337a7eaa126c9a
vladuxa23 <vlad_1995.02@mail.ru>
2023-04-05 21:11:28 +0300

    add figure detector, but with hard example maybe not correct result

--- field.py
+++ field.py
@@ -1,52 +1,18 @@
-from typing import Union, List
+from typing import List
 
 import examples
-from canvas import CanvasPoint, Canvas
-
-
-class FieldPoint(CanvasPoint):
-    def __init__(
-            self, x: Union[int, None] = 0, y: Union[int, None] = 0, fill: str = "*", neighbors: Union[list, None] = None
-    ):
-        super().__init__(x, y, fill)
-
-        self.neighbors = neighbors
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(x={self.x}, y={self.y}, fill={self.fill}, neighbors={self.neighbors})"
-
-    @property
-    def neighbors(self) -> list:
-        """
-        Получение соседей точки
-
-        :return: список объектов FieldPoint
-        """
-
-        return self.__neighbors
-
-    @neighbors.setter
-    def neighbors(self, value: Union[list, None]) -> None:
-        """
-        Установка соседей точки
-
-        :param value: список FieldPoints
-        :return: None
-        """
-
-        if not isinstance(value, (list, type(None))):
-            raise TypeError(f"Expected list or None, received {type(value)}")
-
-        if value is None:
-            self.__neighbors = []
-        else:
-            self.__neighbors = value
+from canvas import Canvas
+from point import FieldPoint, Points
 
 
 class Field:
     def __init__(self, field: str = None):
         self.field_raw = field
         self.__points = self.__init_points(field)
+        self.__points = self.__init_neighbours(self.points)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(field={self.field_raw!r})"
 
     @property
     def width(self) -> int:
@@ -80,6 +46,118 @@ class Field:
 
         return self.__points
 
+    @property
+    def figures(self) -> List[List[FieldPoint]]:
+        figures = []  # создаём список с фигурами
+
+        for x in range(len(self.points)):
+            for y in range(len(self.points[x])):
+                point = self.points[x][y]  # получаем точку из массива
+
+                if not point.neighbours:  # если у точки нет соседей, отбрасываем её
+                    continue
+
+                if not figures:  # для первой точки
+                    figures.append([point, *point.neighbours])
+
+                else:
+                    for figure in figures:
+                        if point in figure:
+                            figure.extend(point.neighbours)
+                            break
+                    else:
+                        figures.append([point, *point.neighbours])
+
+                #
+                # try:
+                #     next_point = self.points[x][y+1]
+                #     if not next_point.neighbours:  # если у следующей точки нет соседей, отбрасываем её
+                #         continue
+                # except IndexError:
+                #     continue
+                #
+                # # for figure in figures:
+                # print(point, next_point)
+                # # --------------------------------------------------------------------------------------------
+                #
+                # # сами точки и их соседи
+                # intersection_result = set(point.neighbours).intersection(set(next_point.neighbours))
+                #
+                # if intersection_result:
+                #     union_points = set(point.neighbours).union(set(next_point.neighbours))
+                #     union_points.add(point)
+                #     union_points.add(next_point)
+                #
+                # print()
+
+                # if intersection_result:
+                #     if not figures:
+                #         figures.append(list(intersection_result))
+                #
+                #     else:
+                #         for figure in figures:
+                #             if point not in figure:
+                #                 for figure_point in figure:
+                #                     if point in figure_point.neighbours:
+                #                         break
+                #                 else:
+                #                     figures.append(list(set(point.neighbours).union(set(next_point.neighbours))))
+                #                     break
+                #
+                #             if next_point not in figure:
+                #                 for figure_point in figure:
+                #                     if next_point in figure_point.neighbours:
+                #                         break
+                #                 else:
+                #                     figures.append(list(set(point.neighbours).union(set(next_point.neighbours))))
+                #                     break
+                        # else:
+                        #     figures.append([point, next_point])
+
+
+
+
+                # -------------------------------------------------
+                #
+                # if not tmp:
+                #     tmp.append(point)
+                #     tmp.extend(point.neighbours)
+                #     tmp = list(set(tmp))
+                #     result.append(tmp)
+                # else:
+                #     for figure in result:
+                #         for neighbour in point.neighbours:
+                #             if neighbour in figure:
+                #                 figure.extend(point.neighbours)
+                #                 figure = list(set(figure))
+                #                 continue
+                #             else:
+                #                 tmp = []
+                #                 tmp.append(neighbour)
+                #                 tmp.extend(neighbour.neighbours)
+                #                 tmp = list(set(tmp))
+                #                 result.append(tmp)
+
+        for i in range(len(figures)):
+            figures[i] = list(set(figures[i]))
+
+        result = []
+
+        for figure in figures:
+            if figure not in result:
+                result.append(figure)
+
+        return result
+
+
+
+    # def __get_figure(self, point: FieldPoint):
+    #     print(point)
+    #     for neighbour in point.neighbours:
+    #
+    #     print(point.neighbours)
+
+
     @staticmethod
     def __init_points(field: str) -> List[FieldPoint]:
         """
@@ -93,18 +171,60 @@ class Field:
 
         rows = field.split("\n")
         points = []
-        for y in range(len(rows[0])):
-            for x in range(len(rows)):
-                points.append(FieldPoint(x, y, fill=rows[x][y], neighbors=None))  # TODO не None, а определение соседей
+        for x in range(len(rows[0])):
+            for y in range(len(rows)):
+                points.append(FieldPoint(x, y, fill=rows[y][x], neighbours=None))  # TODO не None, а определение соседей
+
+        return Points(points)
+
+    def __init_neighbours(self, points: List[FieldPoint]) -> List[FieldPoint]:
+        points = points[:]
+        for row in points:
+            for point in row:
+                neighbours = self.__get_point_neighbour(point)
+                if neighbours:
+                    point.neighbours = self.__get_point_neighbour(point)
 
         return points
 
+    def __get_point_neighbour(self, point: FieldPoint) -> List[FieldPoint]:
+        neighbors = []
+
+        if point.fill == "*":  # TODO Если будет не *, будет не корректно
+            return []
+
+        x = point.x
+        y = point.y
+        potential_neighbors = [
+            (-1, -1), (0, -1), (1, -1),
+            (-1, 0), (1, 0),
+            (-1, 1), (0, 1), (1, 1)
+        ]
+
+        field_raw = self.field_raw.split("\n")
+
+        for p_n in potential_neighbors:
+            try:
+                if field_raw[y + p_n[0]][x + p_n[1]] == ".":
+                    neighbors.append(self.points[x + p_n[1]][y + p_n[0]])
+            except IndexError:
+                pass
+        return neighbors
+
 
 if __name__ == '__main__':
-    field = Field(examples.light)
+    field = Field(examples.hard)
 
-    canvas = Canvas(field.width, field.height)
-    for point in field.points:
-        if point.fill == ".":
-            canvas.draw_point(point.x, point.y)
-    canvas.show()
+    # canvas = Canvas(field.width, field.height)
+    # for row in field.points:
+    #     for point in row:
+    #         if point.fill == ".":
+    #             canvas.draw_point(point.x, point.y)
+
+    for figure in field.figures:
+        canvas = Canvas(field.width, field.height)
+        print()
+        for point in figure:
+            if point.fill == ".":
+                canvas.draw_point(point.x, point.y)
+        canvas.show()
